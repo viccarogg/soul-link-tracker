3ddf2822cb22058f38c40a5578a1956236e3c90e
George <viccarogg@gmail.com>
2023-04-07 21:24:18 -0400

    renames + adding to gitignore

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tracker
+# soul-link-tracker
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tracker'`, () => {
+  it(`should have as title 'soul-link-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tracker');
+    expect(app.title).toEqual('soul-link-tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tracker app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('soul-link-tracker app is running!');
   });
 });
