8833a42beb01246fd7d4294e565e2365f1cfd506
josephkuhn <jrkjosephkuhn@yahoo.com>
2023-11-06 15:49:52 -0500

    encounter tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { PartyComponent } from './party-tab/party/party.component';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PartyTabComponent } from './party-tab/party-tab.component';
+import { EncounterTabComponent } from './encounter-tab/encounter-tab.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PartyComponent,
-    PartyTabComponent
+    PartyTabComponent,
+    EncounterTabComponent
   ],
   imports: [
     MatTabsModule,
